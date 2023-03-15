1ad8d3716a418eb88aa10e823de672f139b9f7f0
Carlos Rojas <j.carlosrojas@outlook.com>
2023-03-14 21:00:06 -0500

    Javascript basico sin funcionalidad

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,15 +3,16 @@ let botonIgual = document.getElementById("botonIgual");
 let salida = document.getElementById("salida");
 
 function calcularPrecioPorKilo() {
-    let pesoProducto = document.getElementById("pesoProducto").value;
+    let pesoProducto = parseFloat(document.getElementById("pesoProducto").value);
     console.log(pesoProducto);
-    let precioProducto = document.getElementById("precioProducto").value;
+    
+    let precioProducto = parseFloat(document.getElementById("precioProducto").value);
     console.log(precioProducto);
 
     let precioPorKilo = (precioProducto*1000)/pesoProducto;
     console.log(precioPorKilo);
 
-    salida.innerHTML = precioPorKilo
+    salida.innerHTML = precioPorKilo.toFixed(2);
   }
 
 botonIgual.addEventListener("click", calcularPrecioPorKilo);
