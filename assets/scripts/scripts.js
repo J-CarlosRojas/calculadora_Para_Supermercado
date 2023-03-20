a1f57ec3c6975f899d96c6de2b47121cd06bc230
Carlos Rojas <j.carlosrojas@outlook.com>
2023-03-19 23:49:12 -0500

    limpieza del codigo

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,17 +3,16 @@ let botonIgual = document.getElementById("botonIgual");
 let botonLimpiar = document.getElementById("botonLimpiar");
 let salida = document.getElementById("salida");
 
+//Funcion para limpiar los imputs y la display de resultado.
+function limpiarInputs() {
+  let limpiarPeso = document.getElementById('pesoProducto');
+  let limpiarPrecio = document.getElementById('precioProducto');
+  limpiarPeso.value = " ";
+  limpiarPrecio.value = " ";
+  salida.innerHTML = " ";
+}
 
-  function limpiarInputs() {
-    let limpiarPeso = document.getElementById('pesoProducto');
-    let limpiarPrecio = document.getElementById('precioProducto');
-
-    limpiarPeso.value = " ";
-    limpiarPrecio.value = " ";
-
-  }
-
-  //seccion de botones
+//seccion de botones
 botonIgual.addEventListener("click", calcularPrecioPorKilo);
 botonLimpiar.addEventListener("click", limpiarInputs);
 
@@ -22,33 +21,22 @@ let select_Cantidad = document.querySelector("#select_Cantidad");
 let cantidad_seleccionada = select_Cantidad.options[select_Cantidad.selectedIndex];
 
 //detector  de cambios en el selector
-select_Cantidad.addEventListener("change", function() {
+select_Cantidad.addEventListener("change", function () {
   cantidad_seleccionada = select_Cantidad.options[select_Cantidad.selectedIndex].value;
   console.log("Nueva cantidad seleccionada: " + cantidad_seleccionada);
 });
 
-
-
+//funcion para calcular precios
 function calcularPrecioPorKilo() {
+  let pesoProducto = parseFloat(document.getElementById("pesoProducto").value);
+  let precioProducto = parseFloat(document.getElementById("precioProducto").value);
 
-  if (cantidad_seleccionada == "gramos"){
-    
-    let pesoProducto = parseFloat(document.getElementById("pesoProducto").value);
-    console.log(pesoProducto);
-    
-    let precioProducto = parseFloat(document.getElementById("precioProducto").value);
-    console.log(precioProducto);
-
-    let precioPorKilo = (precioProducto*1000)/pesoProducto;
-    console.log(precioPorKilo);
-
+  if (cantidad_seleccionada.value == "gramos") {
+    let precioPorKilo = (precioProducto * 1000) / pesoProducto;
     salida.innerHTML = precioPorKilo.toFixed(2);
-  
   } else {
-
-    console.log("selecionado en kilos")
-
+    pesoProducto *= 1000;
+    let precioPorKilo = (precioProducto * 1000) / pesoProducto;
+    salida.innerHTML = precioPorKilo.toFixed(2);
   }
-  
-
 }
