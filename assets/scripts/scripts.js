4d62d9964d9ec26398dcd4ee1cc8696431f0f223
Carlos Rojas <j.carlosrojas@outlook.com>
2023-03-19 22:23:49 -0500

    controlador del selector y cambios de selecion

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,18 +3,6 @@ let botonIgual = document.getElementById("botonIgual");
 let botonLimpiar = document.getElementById("botonLimpiar");
 let salida = document.getElementById("salida");
 
-function calcularPrecioPorKilo() {
-    let pesoProducto = parseFloat(document.getElementById("pesoProducto").value);
-    console.log(pesoProducto);
-    
-    let precioProducto = parseFloat(document.getElementById("precioProducto").value);
-    console.log(precioProducto);
-
-    let precioPorKilo = (precioProducto*1000)/pesoProducto;
-    console.log(precioPorKilo);
-
-    salida.innerHTML = precioPorKilo.toFixed(2);
-  }
 
   function limpiarInputs() {
     let limpiarPeso = document.getElementById('pesoProducto');
@@ -25,5 +13,42 @@ function calcularPrecioPorKilo() {
 
   }
 
+  //seccion de botones
 botonIgual.addEventListener("click", calcularPrecioPorKilo);
 botonLimpiar.addEventListener("click", limpiarInputs);
+
+//controlador del selector
+let select_Cantidad = document.querySelector("#select_Cantidad");
+let cantidad_seleccionada = select_Cantidad.options[select_Cantidad.selectedIndex];
+
+//detector  de cambios en el selector
+select_Cantidad.addEventListener("change", function() {
+  cantidad_seleccionada = select_Cantidad.options[select_Cantidad.selectedIndex].value;
+  console.log("Nueva cantidad seleccionada: " + cantidad_seleccionada);
+});
+
+
+
+function calcularPrecioPorKilo() {
+
+  if (cantidad_seleccionada == "gramos"){
+    
+    let pesoProducto = parseFloat(document.getElementById("pesoProducto").value);
+    console.log(pesoProducto);
+    
+    let precioProducto = parseFloat(document.getElementById("precioProducto").value);
+    console.log(precioProducto);
+
+    let precioPorKilo = (precioProducto*1000)/pesoProducto;
+    console.log(precioPorKilo);
+
+    salida.innerHTML = precioPorKilo.toFixed(2);
+  
+  } else {
+
+    console.log("selecionado en kilos")
+
+  }
+  
+
+}
